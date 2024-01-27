c0bd5b818511c84a1e2c059c40120e95fe9dd848
ibaiAD <ibaiazcona@gmail.com>
2024-01-27 14:02:51 +0100

    1.4: course information step4

--- part-01/courseinfo/src/App.jsx
+++ part-01/courseinfo/src/App.jsx
@@ -20,32 +20,34 @@ const Content = ({ parts }) => {
   );
 };
 
-const Total = ({ totalExercises }) => {
+const Total = ({ parts }) => {
+  const totalExercises = parts.reduce((acc, curr) => acc + curr.exercises, 0);
+
   return <p>Number of exercises {totalExercises}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total
-        totalExercises={part1.exercises + part2.exercises + part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
