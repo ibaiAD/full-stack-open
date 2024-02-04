0ce7733c24652d1c1c1b2da4685e0405552a2b34
ibaiAD <ibaiazcona@gmail.com>
2024-02-04 10:14:15 +0100

    1.11*: unicafe step6

--- part-01/unicafe/src/App.jsx
+++ part-01/unicafe/src/App.jsx
@@ -5,9 +5,10 @@ const Button = ({ handleClick, text }) => {
 };
 
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -21,14 +22,16 @@ const Statistics = ({ good, neutral, bad }) => {
       {total === 0 ? (
         <p>No feedback given</p>
       ) : (
-        <>
-          <StatisticLine text="good" value={good} />
-          <StatisticLine text="neutral" value={neutral} />
-          <StatisticLine text="bad" value={bad} />
-          <StatisticLine text="all" value={total} />
-          <StatisticLine text="average" value={average} />
-          <StatisticLine text="positive" value={positivePercentage + " %"} />
-        </>
+        <table>
+          <tbody>
+            <StatisticLine text="good" value={good} />
+            <StatisticLine text="neutral" value={neutral} />
+            <StatisticLine text="bad" value={bad} />
+            <StatisticLine text="all" value={total} />
+            <StatisticLine text="average" value={average} />
+            <StatisticLine text="positive" value={positivePercentage + " %"} />
+          </tbody>
+        </table>
       )}
     </section>
   );
