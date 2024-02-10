9a451f452f1d94285ae0226a072f443e54bf4cfd
ibaiAD <ibaiazcona@gmail.com>
2024-02-10 17:47:22 +0100

    2.13: The Phonebook step 8

--- part-02/phonebook/src/App.jsx
+++ part-02/phonebook/src/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 
 import { Filter } from "./components/Filter";
 import { PersonForm } from "./components/PersonForm";
 import { Persons } from "./components/Persons";
+import personService from "./services/persons.js";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -12,9 +12,7 @@ const App = () => {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then((response) => setPersons(response.data));
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
   }, []);
 
   const handleSubmit = (e) => {
@@ -30,12 +28,11 @@ const App = () => {
       number: newNumber,
     };
 
-    axios
-      .post("http://localhost:3001/persons", newPerson)
-      .then((response) => setPersons(persons.concat(response.data)));
-
-    setNewName("");
-    setNewNumber("");
+    personService.create(newPerson).then((returnedPerson) => {
+      setPersons(persons.concat(returnedPerson));
+      setNewName("");
+      setNewNumber("");
+    });
   };
 
   const handleFilter = (e) => {
---
+++ part-02/phonebook/src/services/persons.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/persons";
+
+const getAll = async () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const create = async (newPerson) => {
+  const request = axios.post(baseUrl, newPerson);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create };
