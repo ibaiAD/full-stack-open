b1c251cebc826dc82402e9cbcb3c00dd6eeb36cc
ibaiAD <ibaiazcona@gmail.com>
2024-02-16 17:03:01 +0100

    3.5: Phonebook backend step 5

--- part-03/phonebook/index.js
+++ part-03/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -49,6 +51,21 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const { body } = request;
+  const randomId = Math.floor(Math.random() * (1000 - 1) + 1);
+
+  const newPerson = {
+    id: randomId,
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+
+  response.json(newPerson);
 });
 
 const PORT = 3001;
