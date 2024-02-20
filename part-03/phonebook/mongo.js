5bc0b7166802fe834e183a56ea4c23e4845a9801
ibaiAD <ibaiazcona@gmail.com>
2024-02-20 16:24:27 +0100

    3.12: Command-line database

---
+++ part-03/phonebook/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const [password, name, number] = process.argv.filter((_, i) => i > 1);
+
+const url = `mongodb+srv://iad:${password}@cluster0.ih1ooeq.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  Person.find({}).then((persons) => {
+    console.log("phonebook:");
+    persons.forEach((person) => console.log(person.name, person.number));
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
