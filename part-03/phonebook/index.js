524f0387914a61084203b6041e82f5473376a68a
ibaiAD <ibaiazcona@gmail.com>
2024-02-25 19:52:56 +0100

    3.18*: Phonebook database step 6

--- part-03/phonebook/index.js
+++ part-03/phonebook/index.js
@@ -12,34 +12,15 @@ app.use(express.json());
 morgan.token("request-body", (request) => JSON.stringify(request.body));
 app.use(morgan("tiny", { skip: (request) => request.method === "POST" }));
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-app.get("/info", (_, response) => {
-  response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>
-<p>${new Date()}</p>`,
-  );
+app.get("/info", (_, response, next) => {
+  Person.countDocuments()
+    .then((numberOfPeople) => {
+      response.send(`
+<p>Phonebook has info for ${numberOfPeople} people</p>
+<p>${new Date()}</p>
+      `);
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/api/persons", (_, response) => {
@@ -48,13 +29,13 @@ app.get("/api/persons", (_, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (!person) return response.status(404).end();
-
-  response.json(person);
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (!person) return response.status(404).end();
+      return response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response, next) => {
