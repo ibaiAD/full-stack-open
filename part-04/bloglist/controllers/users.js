16e56f7ed62301f09c1e8c30b68ec2041de47fa1
ibaiAD <ibaiazcona@gmail.com>
2024-03-11 17:13:15 +0100

    4.16*: Blog List Expansion, step 4

--- part-04/bloglist/controllers/users.js
+++ part-04/bloglist/controllers/users.js
@@ -2,8 +2,14 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.post('/', async (request, response) => {
+usersRouter.post('/', async (request, response, next) => {
   const { username, name, password } = request.body
+
+  if (!password || password.length < 3) {
+    return response.status(400).json(
+      { error: 'password is required and must be a minimum length of 3' }
+    )
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
@@ -14,9 +20,12 @@ usersRouter.post('/', async (request, response) => {
     passwordHash,
   })
 
-  const savedUser = await user.save()
-
-  response.status(201).json(savedUser)
+  try {
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    next(error)
+  }
 })
 
 usersRouter.get('/', async (request, response) => {
