7cceec4819ca789861fb0d0b6ab813bacacdae71
ibaiAD <ibaiazcona@gmail.com>
2024-03-13 09:01:23 +0100

    4.19: Blog List Expansion, step 7

--- part-04/bloglist/controllers/blogs.js
+++ part-04/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -9,7 +18,19 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
+  let decodedToken
+
+  try {
+    decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  } catch (error) {
+    return next(error)
+  }
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
   const { body } = request
   const { title, url } = body
 
@@ -17,7 +38,7 @@ blogsRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'required parameter missing' })
   }
 
-  const user = await User.findOne({})
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     ...body,
