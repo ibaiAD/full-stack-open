31d4f5d0fe56f2d732204c896ef95d3ba56ab499
ibaiAD <ibaiazcona@gmail.com>
2024-04-02 13:02:29 +0200

    5.15: Blog List Tests, step 3

--- part-05/bloglist-frontend/src/components/Blog.test.jsx
+++ part-05/bloglist-frontend/src/components/Blog.test.jsx
@@ -10,33 +10,58 @@ describe('<Blog />', () => {
     author: 'Tester',
     url: 'https://test-url.com',
     likes: 3,
+    user: {
+      id: '123id456',
+      name: 'TestUser',
+      username: 'tester'
+    }
   }
 
-  beforeEach(() => {
-    container = render(
-      <Blog blog={blog} />
-    ).container
+  describe('<Blog blog={blog} />', () => {
+    beforeEach(() => {
+      container = render(
+        <Blog blog={blog} />
+      ).container
+    })
+
+    test('by default displays title and author but not url and likes', () => {
+      const mainContent = container.querySelector('.mainContent')
+      const togglableContent = container.querySelector('.togglableContent')
+
+      expect(mainContent).not.toHaveStyle('display: none')
+      expect(mainContent).toHaveTextContent(
+        `${blog.title} ${blog.author}`
+      )
+      expect(togglableContent).toHaveStyle('display: none')
+    })
+
+    test('url and number of likes are shown when the button has been clicked', async () => {
+      const user = userEvent.setup()
+      const button = screen.getByText('view')
+      await user.click(button)
+
+      const togglableContent = container.querySelector('.togglableContent')
+      expect(togglableContent).not.toHaveStyle('display: none')
+      expect(togglableContent).toHaveTextContent(blog.url)
+      expect(togglableContent).toHaveTextContent(`likes ${blog.likes}`)
+    })
   })
 
-  test('by default displays title and author but not url and likes', () => {
-    const mainContent = container.querySelector('.mainContent')
-    const togglableContent = container.querySelector('.togglableContent')
+  describe('<Blog blog={blog} updateBlog={updateBlog} />', () => {
+    test('updateBlog is called twice when like button is clicked twice', async () => {
+      const mockHandler = vi.fn()
 
-    expect(mainContent).not.toHaveStyle('display: none')
-    expect(mainContent).toHaveTextContent(
-      `${blog.title} ${blog.author}`
-    )
-    expect(togglableContent).toHaveStyle('display: none')
+      container = render(
+        <Blog blog={blog} updateBlog={mockHandler} />
+      ).container
+
+      const user = userEvent.setup()
+      const button = screen.getByText('like')
+      await user.click(button)
+      await user.click(button)
+
+      expect(mockHandler.mock.calls).toHaveLength(2)
+    })
   })
 
-  test('url and number of likes are shown when the button has been clicked', async () => {
-    const user = userEvent.setup()
-    const button = screen.getByText('view')
-    await user.click(button)
-
-    const togglableContent = container.querySelector('.togglableContent')
-    expect(togglableContent).not.toHaveStyle('display: none')
-    expect(togglableContent).toHaveTextContent(blog.url)
-    expect(togglableContent).toHaveTextContent(`likes ${blog.likes}`)
-  })
 })
