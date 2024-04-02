6bf3e8c6680ca1062074b55c3dc0c5e784c8ddb5
ibaiAD <ibaiazcona@gmail.com>
2024-04-02 12:30:21 +0200

    5.14: Blog List Tests, step 2

--- part-05/bloglist-frontend/src/components/Blog.test.jsx
+++ part-05/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
@@ -26,5 +27,16 @@ describe('<Blog />', () => {
       `${blog.title} ${blog.author}`
     )
     expect(togglableContent).toHaveStyle('display: none')
+  })
+
+  test('url and number of likes are shown when the button has been clicked', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+
+    const togglableContent = container.querySelector('.togglableContent')
+    expect(togglableContent).not.toHaveStyle('display: none')
+    expect(togglableContent).toHaveTextContent(blog.url)
+    expect(togglableContent).toHaveTextContent(`likes ${blog.likes}`)
   })
 })
