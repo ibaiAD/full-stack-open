a4b048eabc093f10b8e7ec48ff986ce7df516503
ibaiAD <ibaiazcona@gmail.com>
2024-04-12 16:50:26 +0200

    6.12 Better Anecdotes, step 10

---
+++ part-06/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = 'initial value'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    changeNotification(_, action) {
+      return action.payload
+    },
+    clearNotification() {
+      return ''
+    }
+  }
+})
+
+export const { changeNotification, clearNotification } = notificationSlice.actions
+export default notificationSlice.reducer
