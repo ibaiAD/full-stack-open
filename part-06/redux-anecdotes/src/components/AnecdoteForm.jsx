bbb27daa683be01735414206366b00e3b9b4713a
ibaiAD <ibaiazcona@gmail.com>
2024-04-17 15:38:19 +0200

    6.17 Anecdotes and the Backend, step 4

--- part-06/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part-06/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useDispatch } from 'react-redux';
 import { createAnecdote } from '../reducers/anecdoteReducer';
 import { changeNotification, clearNotification } from '../reducers/notificationReducer';
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -11,8 +10,7 @@ const AnecdoteForm = () => {
     const content = e.target.anecdote.value
     e.target.anecdote.value = ''
 
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(createAnecdote(newAnecdote))
+    dispatch(createAnecdote(content))
     dispatch(changeNotification(`you added '${content}'`))
     setTimeout(() => {
       dispatch(clearNotification())
--- part-06/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-06/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,9 +5,6 @@ const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
-    createAnecdote(state, action) {
-      state.push(action.payload)
-    },
     addVote(state, action) {
       const id = action.payload
       const votedAnecdote = state.find(a => a.id === id)
@@ -19,18 +16,28 @@ const anecdoteSlice = createSlice({
         anecdote => anecdote.id !== id ? anecdote : changedAnecdote
       )
     },
+    appendAnecdote(state, action) {
+      state.push(action.payload)
+    },
     setAnecdotes(_, action) {
       return action.payload
     },
   }
 })
 
-export const { createAnecdote, addVote, setAnecdotes } = anecdoteSlice.actions
+export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions
 
 export const initializeAnecdotes = () => {
   return async dispatch => {
     const anecdotes = await anecdoteService.getAll()
     dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const newAnecdote = await anecdoteService.createNew(content)
+    dispatch(appendAnecdote(newAnecdote))
   }
 }
 
