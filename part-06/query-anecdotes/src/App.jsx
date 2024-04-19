63452cc5d132b627b6e540eb75702ed40ea73c91
ibaiAD <ibaiazcona@gmail.com>
2024-04-19 14:36:07 +0200

    Exercise 6.22

--- part-06/query-anecdotes/src/App.jsx
+++ part-06/query-anecdotes/src/App.jsx
@@ -1,18 +1,30 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
 
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'],
+        anecdotes.map(a => a.id !== updatedAnecdote.id ? a : updatedAnecdote)
+      )
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
     retry: 1
   })
   console.log(JSON.parse(JSON.stringify(result)))
